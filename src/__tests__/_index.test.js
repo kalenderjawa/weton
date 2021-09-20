7c7bd2ba491406a9357590ba245910ed0a522d4b
Equan Pr <equan.p@gmail.com>
2021-09-20 16:36:21 +0700

    update test

--- src/__tests__/_index.test.js
+++ src/__tests__/_index.test.js
@@ -1,8 +1,16 @@
-import { tabelBobotWeton } from "../index.js";
+import { _LEGI, PASARAN } from "@kalenderjawa/pancawara";
+import { _AKAD, DINTEN } from "@kalenderjawa/saptawara";
+import { tabelBobotWeton, hitungBobotWeton } from "../index.js";
 import assert from "assert";
 
 describe("tabelBobotWeton", () => {
   it("mengembalikan jumlah total weton", () => {
     assert.equal(tabelBobotWeton().length, 35);
+  });
+});
+
+describe("hitungBobotWeton", () => {
+  it("mengembalikan bobot angka suatu weton", () => {
+    assert.equal(hitungBobotWeton(DINTEN.get(_AKAD), PASARAN.get(_LEGI)), 10);
   });
 });
