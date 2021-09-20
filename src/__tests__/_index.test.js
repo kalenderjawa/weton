cb8bd199d1fe7b9eef0aabe63677b4da6da13c8d
Equan Pr <equan.p@gmail.com>
2021-09-20 18:11:52 +0700

    test cari bobot weton dari indeks

--- src/__tests__/_index.test.js
+++ src/__tests__/_index.test.js
@@ -1,6 +1,6 @@
 import { _LEGI, PASARAN } from "@kalenderjawa/pancawara";
 import { _AKAD, DINTEN } from "@kalenderjawa/saptawara";
-import { tabelBobotWeton, hitungBobotWeton } from "../index.js";
+import { tabelBobotWeton, hitungBobotWeton, cariBobotWeton } from "../index.js";
 import assert from "assert";
 
 describe("tabelBobotWeton", () => {
@@ -12,5 +12,15 @@ describe("tabelBobotWeton", () => {
 describe("hitungBobotWeton", () => {
   it("mengembalikan bobot angka suatu weton", () => {
     assert.equal(hitungBobotWeton(DINTEN.get(_AKAD), PASARAN.get(_LEGI)), 10);
+  });
+});
+
+describe("cariBobotWeton", () => {
+  it("mencari bobot weton berdasarkan urutan hari", () => {
+    /**
+     * Urutan hari dimulai dari hari 1 => Senin,
+     * Lihat paket @kalenderjawa/saptawara untuk mengetahui urutan hari.
+     */
+    assert.equal(cariBobotWeton(1, 1), 9);
   });
 });
